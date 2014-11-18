4ccd58c60ad55c6e029056bbc14a8b196e3f5e9d
cyprieng <cyprien.guillemot@gmail.com>
2014-11-17 20:31:51 -0500

    Remove oid

--- app/views.py
+++ app/views.py
@@ -1,6 +1,6 @@
 from flask import render_template, flash, redirect, session, url_for, request, g
 from flask.ext.login import login_user, logout_user, current_user, login_required
-from app import app, db, lm, oid
+from app import app, db, lm
 from .forms import LoginForm, CreateCommunityForm, SearchCommunityForm, CreateShareForm
 from .models import User, Community, Share
 import hashlib
